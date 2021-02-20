888c55c3619c2ea399e7c063b3bf3e9d3d895661
vaaski <admin@vaa.ski>
2021-02-20 20:54:50 +0100

    add dotfiles to package.json files

--- src/dotfiles.ts
+++ src/dotfiles.ts
@@ -2,8 +2,6 @@ import { copy } from "fs-jetpack"
 import { join } from "path"
 
 export default (): void => {
-  // cspell:ignore prettierrc
-
   const toCopy = [".prettierrc", ".eslintrc.json", "license", "tsconfig.json"]
 
   for (const file of toCopy) {
