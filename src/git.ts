610ff8647bb08d51a0857ec146d2acab6145257a
vaaski <admin@vaa.ski>
2021-02-24 12:24:59 +0100

    do not set git remote

--- src/git.ts
+++ src/git.ts
@@ -1,19 +1,9 @@
 import { Input } from "enquirer"
 import execa from "execa"
 
-export default async (name: string): Promise<void> => {
-  const { CREATE_NODE_USERNAME } = process.env
-
+export default async (): Promise<void> => {
   await execa("git", ["init"])
   await execa("git", ["add", "."])
   await execa("git", ["commit", "-m", "init"])
   await execa("git", ["branch", "-M", "main"])
-
-  let repoURL = ""
-  if (CREATE_NODE_USERNAME) repoURL = `https://github.com/${CREATE_NODE_USERNAME}/${name}.git`
-  else {
-    repoURL = await new Input({ message: "repo url: " }).run()
-  }
-
-  if (repoURL) await execa("git", ["remote", "add", "origin", repoURL])
 }
