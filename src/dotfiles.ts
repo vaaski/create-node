2cef0e10d87075957ba51cad0e9150b702e98016
vaaski <admin@vaa.ski>
2021-03-18 20:19:49 +0100

    refactor: rename license to license.md

--- src/dotfiles.ts
+++ src/dotfiles.ts
@@ -2,7 +2,7 @@ import { copy } from "fs-jetpack"
 import { join } from "path"
 
 export default (): void => {
-  const toCopy = [".prettierrc", ".eslintrc.json", "license", "tsconfig.json"]
+  const toCopy = [".prettierrc", ".eslintrc.json", "license.md", "tsconfig.json"]
 
   for (const file of toCopy) {
     copy(join(__dirname, "../", file), file)
