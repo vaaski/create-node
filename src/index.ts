7fc7f55d84d29bb9add94df72c797055fd056381
vaaski <admin@vaa.ski>
2023-02-08 13:30:44 +0100

    feat: validate input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("works")
+import inquirer from "inquirer"
+import { z } from "zod"
+
+const questionSchema = z.object({
+  name: z
+    .string()
+    .min(3, "must be at least 3 characters")
+    .regex(/^[\d_a-z-]+$/, {
+      message: "must be lowercase, numbers, dashes, and underscores",
+    }),
+})
+
+type SchemaShape = typeof questionSchema.shape
+const promptValidator = <T extends keyof SchemaShape>(validator: SchemaShape[T]) => {
+  return (message: string) => {
+    const result = validator.safeParse(message)
+    if (result.success) return true
+
+    const { errors } = result.error
+    const mapped = errors.map(error => error.message).join(",\n")
+    return `error parsing input:\n${mapped}`
+  }
+}
+
+const prompt = await inquirer.prompt([
+  {
+    name: "name",
+    message: "project name:",
+    validate: promptValidator(questionSchema.shape.name),
+  },
+])
+
+console.log("passed", prompt)
