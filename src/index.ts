f10cb43ff6e2d28ab351a65faee1f03eb67ef87f
vaaski <admin@vaa.ski>
2023-02-09 09:12:29 +0100

    feat: zodPrompt

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import inquirer from "inquirer"
 import { z } from "zod"
+import { zodPrompt } from "./prompt"
 
 const questionSchema = z.object({
   name: z
@@ -8,26 +8,9 @@ const questionSchema = z.object({
     .regex(/^[\d_a-z-]+$/, {
       message: "must be lowercase, numbers, dashes, and underscores",
     }),
+  someNumber: z.number().min(0, "must be positive"),
 })
 
-type SchemaShape = typeof questionSchema.shape
-const promptValidator = <T extends keyof SchemaShape>(validator: SchemaShape[T]) => {
-  return (message: string) => {
-    const result = validator.safeParse(message)
-    if (result.success) return true
+const prompt = await zodPrompt(questionSchema, [{ name: "someNumber" }])
 
-    const { errors } = result.error
-    const mapped = errors.map(error => error.message).join(",\n")
-    return `error parsing input:\n${mapped}`
-  }
-}
-
-const prompt = await inquirer.prompt([
-  {
-    name: "name",
-    message: "project name:",
-    validate: promptValidator(questionSchema.shape.name),
-  },
-])
-
-console.log("passed", prompt)
+console.log("passed", prompt.name)
---
+++ src/prompt.ts
@@ -0,0 +1,33 @@
+import type { Question } from "inquirer"
+import inquirer from "inquirer"
+import type { z } from "zod"
+
+type PromptQuestion<Keys> = Question & { name: Keys }
+
+export const zodPrompt = async <RawShape extends z.ZodRawShape>(
+  questionSchema: z.ZodObject<RawShape>,
+  prompts: PromptQuestion<keyof RawShape>[]
+) => {
+  type QuestionKeys = keyof RawShape
+  type SpecificQuestion<K extends QuestionKeys> = Question & { name: K }
+
+  const promptValidator = <K extends QuestionKeys>(validator: RawShape[K]) => {
+    return (message: string) => {
+      const result = validator.safeParse(message)
+      if (result.success) return true
+
+      const { errors } = result.error
+      const mapped = errors.map(error => error.message).join(",\n")
+      return `error parsing input:\n${mapped}`
+    }
+  }
+
+  const autoValidate = <K extends QuestionKeys>(prompt: SpecificQuestion<K>) => {
+    prompt.validate = promptValidator(questionSchema.shape[prompt.name])
+    return prompt
+  }
+
+  const withValidation = prompts.map(element => autoValidate(element))
+  const prompt = await inquirer.prompt(withValidation)
+  return questionSchema.parse(prompt)
+}
